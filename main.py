a148dad7f7d1d062b12c81e2d63422b1838e9967
Jack Cook <hello@jackcook.nyc>
2017-08-12 18:22:12 -0700

    Made sentence endpoint work with individual sentence ids

--- main.py
+++ main.py
@@ -129,43 +129,51 @@ def contributions():
 
 @app.route("/sentences")
 def sentences():
-    query = request.args.get("q")
-    offset = request.args.get("offset")
-    from_lang = request.args.get("from")
+    sid = request.args.get("id")
     
-    offset_num = 0
-    
-    try:
-        offset_num = int(offset)
-    except:
+    if sid is not None:
+        cursor = sql.connection.cursor()
+        cursor.execute("SELECT * FROM sentences WHERE id = " + sid)
+        data = Sentence(cursor.fetchall()[0]).dict()
+        return jsonify(data)
+    else:
+        query = request.args.get("q")
+        offset = request.args.get("offset")
+        from_lang = request.args.get("from")
+        
         offset_num = 0
         
-    where_languages = ""
-    
-    if from_lang is not None:
-        where_languages = " AND lang = '" + from_lang + "'"
-    
-    cursor = sql.connection.cursor()
-    cursor.execute("SELECT * FROM sentences WHERE text LIKE '%" + query + "%'" + where_languages + " LIMIT 10 OFFSET " + str(offset_num))
-    result = cursor.fetchall()
-    sentences = []
-    
-    for row in result:
-        sentence = Sentence(row)
-        cursor.execute("SELECT * FROM sentences_translations WHERE sentence_id = %d" % sentence.id)
-        translations_result = cursor.fetchall()
-        translations = []
+        try:
+            offset_num = int(offset)
+        except:
+            offset_num = 0
+            
+        where_languages = ""
         
-        for translation_row in translations_result:
-            translation = Translation(translation_row)
-            cursor.execute("SELECT * FROM sentences WHERE id = %d" % translation.translation_id)
-            translation_sentence = Sentence(cursor.fetchall()[0])
-            translations.append(translation_sentence.dict())
+        if from_lang is not None:
+            where_languages = " AND lang = '" + from_lang + "'"
         
-        sentence.translations = translations
-        sentences.append(sentence.dict())
-    
-    return jsonify(sentences)
+        cursor = sql.connection.cursor()
+        cursor.execute("SELECT * FROM sentences WHERE text LIKE '%" + query + "%'" + where_languages + " LIMIT 10 OFFSET " + str(offset_num))
+        result = cursor.fetchall()
+        sentences = []
+        
+        for row in result:
+            sentence = Sentence(row)
+            cursor.execute("SELECT * FROM sentences_translations WHERE sentence_id = %d" % sentence.id)
+            translations_result = cursor.fetchall()
+            translations = []
+            
+            for translation_row in translations_result:
+                translation = Translation(translation_row)
+                cursor.execute("SELECT * FROM sentences WHERE id = %d" % translation.translation_id)
+                translation_sentence = Sentence(cursor.fetchall()[0])
+                translations.append(translation_sentence.dict())
+            
+            sentence.translations = translations
+            sentences.append(sentence.dict())
+        
+        return jsonify(sentences)
 
 if __name__ == "__main__":
     app.run(debug=True)
